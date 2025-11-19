9d8d9f7ad01ec13d5181bc71e46430ebb89239e8
winsonteo <winsonteobh@gmail.com>
2025-11-19 23:00:25 +0800

    Set static disallow-all robots for beta

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,18 +1,7 @@
 import type { MetadataRoute } from "next";
-import { headers } from "next/headers";
 
 export default function robots(): MetadataRoute.Robots {
-  const headersList = headers();
-  const host = headersList.get("host")?.toLowerCase() ?? "";
-
-  if (host === "beta.griprank.com") {
-    return {
-      rules: [{ userAgent: "*", disallow: "/" }],
-    };
-  }
-
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://griprank.com/sitemap.xml",
+    rules: [{ userAgent: "*", disallow: "/" }],
   };
 }
